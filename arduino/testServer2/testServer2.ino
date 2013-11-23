daa60d4a050ad67d6ffff2d0cccf143e88c578b0
Jean-Claude Wippler <jc@wippler.nl>
2013-11-23 14:06:33 +0100

    cleanup

--- arduino/testServer2/testServer2.ino
+++ arduino/testServer2/testServer2.ino
@@ -3,10 +3,10 @@
 // 2012-10-24 <jcw@equi4.com> http://opensource.org/licenses/mit-license.php
 
 #include <JeeLib.h>
-// #include <JeeBoot.h>
-#include <avr/pgmspace.h>
 #include <util/crc16.h>
 
+// struct { const char* title; unsigned start, off, count; } sections[];
+// const unsigned char progdata[] PROGMEM = ...
 #include "data_blinks.h"
 
 #define BOOT_DATA_MAX 64
@@ -28,81 +28,6 @@ static word calcCRCrom (const void* ptr, word len) {
   return crc;
 }
 
-#if 0
-BootReply bootReply;
-DataReply dataReply;
-
-byte sectNum;
-
-static void sendReply (const void* ptr, byte len) {
-  bitSet(DDRB, 1);
-  bitClear(PORTB, 1);
-  rf12_sendStart(RF12_ACK_REPLY, ptr, len);
-  rf12_sendWait(2);
-  bitSet(PORTB, 1);
-}
-
-static void initialRequest (word rid) {
-  sectNum = 1 - sectNum; // send a different sketch out each time
-
-  bootReply.remoteID = 0;
-  bootReply.sketchBlocks = (sections[sectNum].count + 63) / 64;
-  word len = 64 * bootReply.sketchBlocks;
-  bootReply.sketchCRC = calcCRCrom(progdata + sections[sectNum].off, len);
-  
-#if DEBUG        
-  Serial.print("ir ");
-  Serial.print(rid);
-  Serial.print(" sb ");
-  Serial.print(bootReply.sketchBlocks);
-  Serial.print(" crc ");
-  Serial.println(bootReply.sketchCRC);
-  Serial.flush();
-#endif
-
-#if DEBUG > 1
-  Serial.print(" -> ack ");
-  Serial.println(bootReply.sketchBlocks, DEC);
-  
-  Serial.print("  send:");
-  for (byte i = 0; i < sizeof bootReply; ++i) {
-    Serial.print(' ');
-    Serial.print(((byte*) &bootReply)[i], HEX);
-  }
-  Serial.println();
-#endif
-      
-  sendReply(&bootReply, sizeof bootReply);
-}
-
-static void dataRequest (word rid, word blk) {
-  dataReply.info = blk ^ rid;
-  word pos = 64 * blk;
-
-#if DEBUG        
-  Serial.print("dr ");
-  Serial.print(rid);
-  Serial.print(" # ");
-  Serial.print(blk);
-#if DEBUG > 1
-  Serial.print(" -> ");
-  Serial.print(dataReply.info);
-  Serial.print(" @ ");
-  Serial.print(pos);
-#endif
-  Serial.println();
-  Serial.flush();
-#endif
-  
-  for (byte i = 0; i < 64; ++i) {
-    byte d = pgm_read_byte(progdata + sections[sectNum].off + pos + i);
-    dataReply.data[i] = d; // ^ ...
-  }
-
-  sendReply(&dataReply, sizeof dataReply);
-}
-#endif
-
 static void* memcpy(void* dst, const void* src, int len) {
   uint8_t* to = (uint8_t*) dst;
   const uint8_t* from = (const uint8_t*) src;
@@ -119,10 +44,8 @@ void setup () {
   rf12_initialize(31, RF12_868MHZ, 212);
 }
 
-// struct { const char* title; unsigned start, off, count; } sections[];
-// const unsigned char progdata[] PROGMEM = ...
-
 void loop () {
+	// boot loader request packets haev a special header with CTL *and* ACK set
   if (rf12_recvDone() && rf12_crc == 0 &&
       (rf12_hdr & RF12_HDR_CTL) && (rf12_hdr & RF12_HDR_ACK)) {
     switch (rf12_len) {
@@ -130,7 +53,7 @@ void loop () {
         Serial.print(F("bad length: "));
         Serial.println(rf12_len);
         break;
-      case 22: {
+      case 22: { // packets of length 22 are pairing requests
         struct PairingRequest *reqp = (struct PairingRequest*) rf12_data;
         Serial.print(F("announce type 0x"));
         Serial.println(reqp->type, HEX);
@@ -146,7 +69,7 @@ void loop () {
         rf12_sendNow(0, &reply, sizeof reply);
         break;
       }
-      case 8: {
+      case 8: { // packets of length 8 are upgrade requests
         struct UpgradeRequest *reqp = (struct UpgradeRequest*) rf12_data;
         Serial.print(F("boot swId 0x"));
         Serial.println(reqp->swId, HEX);
@@ -165,7 +88,7 @@ void loop () {
         rf12_sendNow(RF12_HDR_DST | 17, &reply, sizeof reply);
         break;
       }
-      case 4: {
+      case 4: { // packets of length 22 are download requests
         struct DownloadRequest *reqp = (struct DownloadRequest*) rf12_data;
         Serial.print(F("data swIndex "));
         Serial.println(reqp->swIndex);
@@ -188,10 +111,5 @@ void loop () {
         break;
       }
     }
-    // const word* args = (const word*) rf12_data;
-    // if (rf12_len == 2)
-    //   initialRequest(args[0]);
-    // else if (rf12_len == 4)
-    //   dataRequest(args[0], args[1]);
   }
 }
