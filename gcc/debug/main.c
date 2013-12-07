b3caea93440de5df896fecb184212c4942610c72
Jean-Claude Wippler <jc@wippler.nl>
2013-12-07 19:21:22 +0100

    tweak

--- gcc/debug/main.c
+++ gcc/debug/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "uart.h"
 
+const int ledPin = 7;
+
 volatile uint32_t msTicks;
 
 void SysTick_Handler (void) {
@@ -32,12 +34,12 @@ int main (void) {
   uart0Init(9600);
   
   SysTick_Config(__SYSTEM_CLOCK/1000);   // 1000 Hz
-  LPC_GPIO_PORT->DIR0 |= (1 << 7);
+  LPC_GPIO_PORT->DIR0 |= (1 << ledPin);
 
   printf("clock %d\r\n", __SYSTEM_CLOCK);
     
   while (1) {
-    LPC_GPIO_PORT->NOT0 = 1 << 7;
+    LPC_GPIO_PORT->NOT0 = 1 << ledPin;
     delay_ms(500);
   }
   
