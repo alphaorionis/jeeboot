53b19f40b424718030c952ee87fb8e6e3931a10d
Jean-Claude Wippler <jc@wippler.nl>
2013-12-12 02:08:30 +0100

    cleanup

--- gcc/base/uart_int.c
+++ gcc/base/uart_int.c
@@ -21,7 +21,7 @@ typedef struct {
 static RingBuffer rxBuf, txBuf;
 
 static void rbInit (RingBuffer* rb) {
-  rb->fill = rb->take = 0;
+  // rb->fill = rb->take = 0;
 }
 
 static int rbIsEmpty (RingBuffer* rb) {
@@ -49,7 +49,6 @@ void uart0Init (uint32_t baudRate) {
   rbInit(&rxBuf);
   rbInit(&txBuf);
   
-  uint32_t clk;
   const uint32_t UARTCLKDIV=1;
 
   /* Setup the clock and reset UART0 */
@@ -60,7 +59,7 @@ void uart0Init (uint32_t baudRate) {
   LPC_SYSCON->PRESETCTRL    |=  (1 << 3);
 
   /* Configure UART0 */
-  clk = __SYSTEM_CLOCK / UARTCLKDIV;
+  uint32_t clk = __SYSTEM_CLOCK / UARTCLKDIV;
   LPC_USART0->CFG = UART_DATA_LENGTH_8 | UART_PARITY_NONE | UART_STOP_BIT_1;
   LPC_USART0->BRG = clk / 16 / baudRate - 1;
   LPC_SYSCON->UARTFRGDIV = 0xFF;
