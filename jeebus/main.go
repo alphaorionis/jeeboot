68bf5a5150a19a5eedd874431f3ba6f2756cc0f2
Jean-Claude Wippler <jc@wippler.nl>
2014-01-27 00:53:37 +0100

    add packet definitions and decoding

--- jeebus/main.go
+++ jeebus/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -10,6 +13,9 @@ import (
 
 	"github.com/jcw/jeebus"
 )
+
+const CONFIG_FILE = "./config.json"   // boot server configuration file
+const FIRMWARE_PREFIX = "./firmware/" // location of hex files to serve
 
 func main() {
 	if len(os.Args) <= 1 {
@@ -28,26 +34,44 @@ func main() {
 
 func boots(dev string) {
 	rdClient := jeebus.NewClient("rd")
-	rdClient.Register("RF12demo/#", &JeeBootService{dev})
+	rdClient.Register("RF12demo/"+dev, &JeeBootService{dev, loadConfig()})
 
 	var msg jeebus.Message
 	msg.Set("text", "8b 212g 31i 1c")
 	msg.Publish("if/RF12demo/" + dev)
 
+	// TODO need a mechanism to wait for client disconnect, possibly w/ retries
 	<-make(chan byte) // wait forever
 }
 
+type Config struct {
+	// map 16-byte hardware ID to the assigned pairing info
+	HwIds map[string]struct {
+		Type, Group, NodeId float64
+	}
+	// names for each of the node types
+	Types map[string]string
+	// name nodetype-group-nodeid to a map of swId's to filenames
+	Nodes map[string]map[string]string
+}
+
+func loadConfig() (config Config) {
+	data, err := ioutil.ReadFile(CONFIG_FILE)
+	check(err)
+	err = json.Unmarshal(data, &config)
+	check(err)
+	log.Printf("CONFIG %+v", config)
+	return
+}
+
 type JeeBootService struct {
-	dev string
+	dev    string
+	config Config
 }
 
 func (s *JeeBootService) Handle(m *jeebus.Message) {
-	dev := strings.SplitN(m.T, "/", 2)[1]
-	if dev != s.dev {
-		return // not the device used for JeeBoot, ignore it
-	}
 	text := m.Get("text")
-	log.Print(text)
+	// log.Print(text)
 	if strings.HasPrefix(text, "OK ") {
 		var buf bytes.Buffer
 		// convert the line of decimal byte values to a byte buffer
@@ -56,9 +80,77 @@ func (s *JeeBootService) Handle(m *jeebus.Message) {
 			check(err)
 			buf.WriteByte(byte(n))
 		}
-		now := m.GetInt64("time")
-		dev := strings.SplitN(m.T, "/", 2)[1]
-		fmt.Printf("%d %s %X\n", now, dev, buf.Bytes())
+		s.RespondToRequest(buf.Bytes())
+	}
+}
+
+type PairingRequest struct {
+	Header  uint8
+	Variant uint8     // variant of remote node, 1..250 freely available
+	Board   uint8     // type of remote node, 100..250 freely available
+	Group   uint8     // current network group, 1..250 or 0 if unpaired
+	NodeId  uint8     // current node ID, 1..30 or 0 if unpaired
+	Check   uint16    // crc checksum over the current shared key
+	HwId    [16]uint8 // unique hardware ID or 0's if not available
+}
+
+type PairingReply struct {
+	Variant uint8     // variant of remote node, 1..250 freely available
+	Board   uint8     // type of remote node, 100..250 freely available
+	Group   uint8     // assigned network group, 1..250
+	NodeId  uint8     // assigned node ID, 1..30
+	ShKey   [16]uint8 // shared key or 0's if not used
+}
+
+type UpgradeRequest struct {
+	Header  uint8
+	Variant uint8  // variant of remote node, 1..250 freely available
+	Board   uint8  // type of remote node, 100..250 freely available
+	SwId    uint16 // current software ID 0 if unknown
+	SwSize  uint16 // current software download size, in units of 16 bytes
+	SwCheck uint16 // current crc checksum over entire download
+}
+
+type UpgradeReply struct {
+	Variant uint8  // variant of remote node, 1..250 freely available
+	Board   uint8  // type of remote node, 100..250 freely available
+	SwId    uint16 // assigned software ID
+	SwSize  uint16 // software download size, in units of 16 bytes
+	SwCheck uint16 // crc checksum over entire download
+}
+
+type DownloadRequest struct {
+	Header  uint8
+	SwId    uint16 // current software ID
+	SwIndex uint16 // current download index, as multiple of payload size
+}
+
+type DownloadReply struct {
+	SwIdXor uint16    // current software ID xor current download index
+	Data    [64]uint8 // download payload
+}
+
+func (s *JeeBootService) RespondToRequest(req []byte) {
+	// fmt.Printf("%s %X %d\n", s.dev, req, len(req))
+	reader := bytes.NewReader(req)
+	switch len(req) {
+	case 23:
+		var preq PairingRequest
+		err := binary.Read(reader, binary.LittleEndian, &preq)
+		fmt.Printf("%+v\n", &preq)
+		check(err)
+	case 9:
+		var ureq UpgradeRequest
+		err := binary.Read(reader, binary.LittleEndian, &ureq)
+		fmt.Printf("%+v\n", &ureq)
+		check(err)
+	case 5:
+		var dreq DownloadRequest
+		err := binary.Read(reader, binary.LittleEndian, &dreq)
+		fmt.Printf("%+v\n", &dreq)
+		check(err)
+	default:
+		log.Printf("bad req? %db = %X", len(req), req)
 	}
 }
 
