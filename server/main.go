270ca7047a2ae9a5fc8c14bb4f427b9a83153365
Jean-Claude Wippler <jc@wippler.nl>
2014-02-23 15:21:05 +0100

    cleanup

--- server/main.go
+++ server/main.go
@@ -5,7 +5,7 @@ import (
     "github.com/jcw/jeebus"
 )
 
-const Version = "0.0.1"
+const Version = "0.2.0"
 
 func init() {
     log.SetFlags(log.Ltime) // only display HH:MM:SS time in log entries
