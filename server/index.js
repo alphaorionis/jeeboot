4b980babb88a0cd6bad2dc639501e6b88ad98d8c
Jean-Claude Wippler <jc@wippler.nl>
2014-02-23 22:07:16 +0100

    fix hardcoded path

--- server/index.js
+++ server/index.js
@@ -1,1 +1,1 @@
-require('/Users/jcw/src/github.com/jcw/jeebus/common/devmode');
+require('../../jeebus/common/devmode');
--- server/main.go
+++ server/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/jcw/jeebus"
 
 func main() {
-    jeebus.Run()
+	jeebus.Run()
 }
