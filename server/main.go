123ae50a3c6e104a4113c2109b9b93810855b8e6
Jean-Claude Wippler <jc@wippler.nl>
2014-02-23 19:16:31 +0100

    use default log settings

--- server/main.go
+++ server/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-    "log"
     "github.com/jcw/jeebus"
 )
 
 const Version = "0.2.0"
-
-func init() {
-    log.SetFlags(log.Ltime) // only display HH:MM:SS time in log entries
-}
 
 func main() {
     println("\nJeeBoot Server", Version, "/ JeeBus", jeebus.Version)
