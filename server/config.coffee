c681cf598dcf691376dc964c1037344b55222912
Jean-Claude Wippler <jc@wippler.nl>
2014-03-17 01:45:42 +0100

    add boot server file loading pipeline

--- server/config.coffee
+++ server/config.coffee
@@ -1,12 +1,11 @@
 #!/usr/bin/env coffee
 # Generate hard-to-edit JSON config file from an easy-to-edit CoffeeScript one.
-config =
-  files: '../firmware'
+config = {}
 
 config.swids =
-  1001: 'blinkAvr1.hex'
-  1002: 'blinkAvr2.hex'
-  1003: 'blinkAvr3.hex'
+  1001: '../firmware/blinkAvr1.hex'
+  1002: '../firmware/blinkAvr2.hex'
+  1003: '../firmware/blinkAvr3.hex'
 	
 config.hwids =
   '06300301c48461aeedb09351061900f5':
--- server/main.go
+++ server/main.go
@@ -25,8 +25,6 @@ var (
 		"net group used to listen for incoming JeeBoot requests")
 	configFile = flag.String("config", "config.json",
 		"configuration file containing the swid/hwid details")
-	hexFiles = flag.String("firmware", "../firmware",
-		"location of the Intel hex firware files to serve")
 )
 
 func main() {
@@ -45,27 +43,33 @@ func main() {
 
 	// main flow is: serial -> rf12demo -> jeeboot -> serial
 	// other valid packets are routed to the bootServer gadget
-	
+
 	c := flow.NewCircuit()
 	c.Add("sp", "SerialPort")
 	c.Add("rf", "Sketch-RF12demo")
 	c.Add("sk", "Sink")
 	c.Add("jb", "JeeBoot")
+	c.Add("rd", "ReadTextFile")
+	c.Add("hx", "IntelHexToBin")
+	c.Add("bf", "BinaryFill")
+	c.Add("cs", "CalcCrc16")
+	c.Add("bd", "BootData")
 	c.AddCircuitry("server", &bootServer{})
 	c.Connect("sp.From", "rf.In", 0)
 	c.Connect("rf.Out", "server.In", 0)
 	c.Connect("rf.Rej", "sk.In", 0) // throw away rejected serial port msgs
 	c.Connect("rf.Oob", "jb.In", 0)
+	c.Connect("jb.Files", "rd.In", 0)
+	c.Connect("rd.Out", "hx.In", 0)
+	c.Connect("hx.Out", "bf.In", 0)
+	c.Connect("bf.Out", "cs.In", 0)
+	c.Connect("cs.Out", "bd.In", 0)
 	c.Connect("jb.Out", "sp.To", 0)
 	c.Connect("server.Out", "sp.To", 0)
 	c.Feed("sp.Port", *serialPort)
 	c.Feed("jb.Cfg", config)
+	c.Feed("bf.Len", 64)
 	c.Run()
-}
-
-type configData struct {
-	SwIDs map[string]string
-	HwIDs map[string]struct{ Board, Group, Node, SwID int }
 }
 
 type bootServer struct {
@@ -77,7 +81,6 @@ type bootServer struct {
 func (g *bootServer) Run() {
 	g.Out.Send(1) // reset the serial port
 	<-g.In        // wait for some input before sending out the init command
-	<-g.In
 	g.Out.Send(fmt.Sprintf("%db %dg 31i 1c 1q v", *freqBand/100, *netGroup))
 
 	for m := range g.In {
